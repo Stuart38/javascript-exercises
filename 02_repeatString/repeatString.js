f147786a523d35f98f8d71561182439ca4ede901
Stuart Anderson <r.stuart.anderson@gmail.com>
2023-02-10 05:38:35 -0500

    Completed reverse string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,7 +3,18 @@ const repeatString = function (string, num) {
     return string.repeat(num);
 };
 
+// Another method: 
 
+// const repeatString = function(word, times) {
+//   if (times < 0) return 'ERROR';
+//   let string = '';
+//   for (let i = 0; i < times; i++) {
+//     string += word;
+//   }
+//   return string;
+// };
+
+// module.exports = repeatString;
 
 
 
