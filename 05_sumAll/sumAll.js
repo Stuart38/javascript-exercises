980851f387e6a7cadb3a746b154446f19028c6c0
Stuart Anderson <r.stuart.anderson@gmail.com>
2023-04-29 15:03:47 -0400

    Finished sumall exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    let total = 0;
+    if (x <= 0 || y <= 0) {
+        return 'ERROR';
+    } else if (Number.isInteger(x) === false || Number.isInteger(y) === false) {
+        return 'ERROR';
+    } else if (x > y) {
+        for (i = y; i <= x; i++) {
+            total += i;
+        }
+        return (total);
+    }
+    for (i = x; i <= y; i++) {
+        total += i;
+    }
+    return (total);
 };
 
 // Do not edit below this line
