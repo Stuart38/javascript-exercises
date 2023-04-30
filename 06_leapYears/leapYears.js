fee785cdf62ecbc6d60d9ce1809db8b46069e409
Stuart Anderson <r.stuart.anderson@gmail.com>
2023-04-30 11:41:07 -0400

    Fnished the leapyear exercises.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+// const leapYears = function(year) {
+//     let result = 0;
+//     if (year % 4 == 0 && year % 100 !== 0)  {
+//     } if (year % 400 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
 
+const leapYears = function (year) {
+    let result = 0;
+    if (year % 4 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
