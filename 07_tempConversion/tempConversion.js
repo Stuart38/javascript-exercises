9aefd62ba9555707edd463161326006d67b9842c
Stuart Anderson <r.stuart.anderson@gmail.com>
2023-04-30 12:58:39 -0400

    Completed temp conversion exercise.  Nice!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let result = (temp - 32) * (5 / 9);
+  return Math.round(result * 1e1) / 1e1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let result = (temp * 1.8) + 32;
+  return Math.round(result * 1e1) / 1e1;
 };
 
 // Do not edit below this line
